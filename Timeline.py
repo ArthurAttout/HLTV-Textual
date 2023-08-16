fceb26600f306b62bbb119c6548163e43ec6480b
Arthur ATTOUT <arthur.attout@outlook.com>
2023-08-16 21:04:10 +0200

    Added emoji for round ending status

--- Timeline.py
+++ Timeline.py
@@ -1,16 +1,55 @@
 from __future__ import annotations
-from textual.app import App, ComposeResult
-from textual.containers import Container, Horizontal
-from textual.widgets import Footer, Header, Static, Placeholder
+from textual.app import ComposeResult
+from textual.containers import Container, Vertical
+from textual.widgets import Static, Placeholder
+from textual.widget import Widget
 import random
+from enum import Enum
 random.seed(42)
+
+class RoundResult(Enum):
+    BOMB_DEFUSED = 1
+    BOMB_EXPLODED = 2
+    T_SIDE_KILLED = 3
+    CT_SIDE_KILLED = 4
+
+class RoundResultComponent(Static):
+    
+    def __init__(self, result: RoundResult):
+        Static.__init__(self)
+        self.result = result
+
+    def compose(self) -> ComposeResult:
+        if self.result == RoundResult.BOMB_DEFUSED:
+            yield Vertical(
+                Static("🔧", classes="round-result-cell"),
+                Static(classes="round-result-cell")
+            )
+        elif self.result == RoundResult.T_SIDE_KILLED:
+            yield Vertical(
+                Static("💀", classes="round-result-cell"),
+                Static(classes="round-result-cell")
+            )
+        elif self.result == RoundResult.BOMB_EXPLODED:
+            yield Vertical(
+                Static(classes="round-result-cell"),
+                Static("💥", classes="round-result-cell")
+            )
+        elif self.result == RoundResult.CT_SIDE_KILLED:
+            yield Vertical(
+                Static(classes="round-result-cell"),
+                Static("💀", classes="round-result-cell")
+            )
+        else:
+            yield Static("Wtf ?", classes="round-result-cell")
 
 class Timeline(Static):
     def compose(self) -> ComposeResult:
+        generator = RoundGenerator(30)
         with Container(id="timeline-group"):
-            generator = RoundGenerator(30)
-            for is_ct_win in generator:
-                yield Placeholder("CT" if is_ct_win else "T")
+            for result in generator:
+               yield RoundResultComponent(result)
+    
 
 
 class RoundGenerator:
@@ -18,16 +57,16 @@ class RoundGenerator:
     def __init__(self, number_rounds):
         self.number_rounds = number_rounds
         self.current_iteration = 0
-        
 
-    def __next__(self):
+    def __next__(self) -> RoundResult:
         return self.next()
     
-    def __iter__(self):
+    def __iter__(self) -> RoundResult:
         return self
 
-    def next(self):
+    def next(self) -> RoundResult:
         if self.current_iteration < self.number_rounds:
             self.current_iteration = self.current_iteration + 1
-            return random.randint(0,10) < 5
+            index = random.randint(0,len(RoundResult) - 1)
+            return list(RoundResult)[index]
         raise StopIteration()
