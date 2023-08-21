dc9cfc170ca79e6b9a626c3168818a10b692ce33
Arthur ATTOUT <arthur.attout@outlook.com>
2023-08-21 20:45:42 +0200

    Try out reactives, but honestly they suck : use plain attributes with .update() method

--- Scoreboard.py
+++ Scoreboard.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
 from textual.app import App, ComposeResult
+from textual.reactive import reactive
 from textual.containers import Container, Horizontal
 from textual.widgets import Footer, Header, Static, Placeholder
 
 from ScoreboardTopHeader import ScoreboardTopHeader
 from TeamStats import TeamStats
 from Timeline import Timeline
-
+from ScoreboardTopHeader import ScoreboardData
 from TeamStats import PlayerStat
 
-ROWS = [
+TEAM_STATS = [
     PlayerStat("deko",        False, "WP",100,True,1430,4,1,2,87.0),
     PlayerStat("Boombl4",     False,"AWP",100,True,1430,4,1,2,87.0),
     PlayerStat("TRAVIS",      True,"AWP",100,True,1430,4,1,2,87.0),
@@ -17,17 +18,37 @@ ROWS = [
     PlayerStat("Forester",    True,"AWP",100,True,1430,4,1,2,87.0),
 ]
 
+SCOREBOARD_DATA = ScoreboardData(13, "Overpass", "10", 4, 8)
 
 class ScoreboardApp(App):
+    counter = 0
     CSS_PATH = "Scoreboard.css"
+
+    def __init__(self):
+        App.__init__(self)
+
     def compose(self) -> ComposeResult:
         yield Header()
-        yield ScoreboardTopHeader()
-        yield TeamStats(ROWS)
+        yield ScoreboardTopHeader(SCOREBOARD_DATA)
+        yield TeamStats(TEAM_STATS)
         yield Timeline()
-        yield TeamStats(ROWS)
+        yield TeamStats(TEAM_STATS)
         yield Footer()
 
+    def on_mount(self) -> None:
+        self.set_interval(1, self.update_data)
+
+    def update_data(self):
+        self.counter += 1
+        new_data = ScoreboardData(
+            str(self.counter + 13), 
+            "Overpass", 
+            str(self.counter), 
+            str(self.counter + 4), 
+            str(self.counter + 9)
+        )
+        self.query_one(ScoreboardTopHeader).update_data(new_data)
+        
 
 if __name__ == "__main__":
     app = ScoreboardApp()
--- ScoreboardTopHeader.py
+++ ScoreboardTopHeader.py
@@ -2,19 +2,45 @@ from __future__ import annotations
 from textual.app import App, ComposeResult
 from textual.containers import Container, Horizontal
 from textual.widgets import Footer, Header, Static, Placeholder
-
+from textual.reactive import reactive
 
 class TeamsScores(Static):
+    def __init__(self, data:ScoreboardData):
+        Static.__init__(self)
+        self.ct_score = reactive(data.ct_score)
+        self.t_score = reactive(data.t_score)
+
     def compose(self) -> ComposeResult:
         with Container(id="teams-score"):
-            yield Static("10",id="score-ct", classes="scoreboard-element")
+            yield Static("0",id="score-ct", classes="scoreboard-element")
             yield Static(" - ", id="score-separator", classes="scoreboard-element")
-            yield Static("9",id="score-t", classes="scoreboard-element")
+            yield Static("0",id="score-t", classes="scoreboard-element")
+
+    def update_data(self, data:ScoreboardData):
+        self.query_one("#score-ct", Static).update(data.ct_score)
+        self.query_one("#score-t", Static).update(data.t_score)
 
 class ScoreboardTopHeader(Static):
+    def __init__(self,data:ScoreboardData):
+        Static.__init__(self)
+        self.data = data
+        
     def compose(self) -> ComposeResult:
         with Container(id="scoreboard-container"):
-            yield Placeholder("R27 - Overpass",classes="scoreboard-element")
-            yield TeamsScores()
-            yield Placeholder("0:56",classes="scoreboard-element")
+            yield Static("", id="elem-scoreboard-round", classes="scoreboard-round")
+            yield TeamsScores(self.data)
+            yield Static("0", id="elem-scoreboard-time", classes="scoreboard-time")
+    
+    def update_data(self, data:ScoreboardData):
+        round_status = f"{data.map_name} - {data.round_number}"
+        self.query_one(TeamsScores).update_data(data)
+        self.query_one("#elem-scoreboard-time", Static).update(data.time)
+        self.query_one("#elem-scoreboard-round", Static).update(round_status)
 
+class ScoreboardData:
+    def __init__(self, round_number:int, map_name:str, time:str, ct_score:int, t_score:int):
+        self.round_number = str(round_number)
+        self.map_name = map_name
+        self.time = str(time)
+        self.ct_score = str(ct_score)
+        self.t_score = str(t_score)
